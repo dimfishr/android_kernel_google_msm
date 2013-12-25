62efcd0a797971dd40e84b7e045ad164a17343c9
dimfishr <dimfish@gmail.com>
2013-12-25 12:29:56 +0400

    Intelli-plug activate 2 cores when no EcoMode
    Signed-off-by: dimfishr <dimfish@gmail.com>

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -21,6 +21,7 @@
 #include <linux/rq_stats.h>
 #include <linux/slab.h>
 #include <linux/input.h>
+#include <linux/cpufreq.h>
 #include <linux/kobject.h>
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/earlysuspend.h>
@@ -306,29 +307,33 @@ static unsigned int calculate_thread_stats(void)
 {
 	unsigned int avg_nr_run = avg_nr_running();
 	unsigned int nr_run;
+	unsigned int nr_run_min;
 	unsigned int threshold_size;
 
 	if (eco_mode_active == 1) {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_eco);
 		nr_run_hysteresis = 4;
 		nr_fshift = 1;
+		nr_run_min = 1;
 		if (debug_intelli_plug)
 			pr_info("intelliplug: eco mode active!");
 	} else if (strict_mode_active == 1) {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_strict);
 		nr_run_hysteresis = 2;
 		nr_fshift = 1;
+		nr_run_min = 1;
 		if (debug_intelli_plug)
 			pr_info("intelliplug: strict mode active!");
 	} else {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_full);
 		nr_run_hysteresis = 8;
 		nr_fshift = 3;
+		nr_run_min = mako_boosted ? 2 : 1;
 		if (debug_intelli_plug)
 			pr_info("intelliplug: full mode active!");
 	}
 
-	for (nr_run = 1; nr_run < threshold_size; nr_run++) {
+	for (nr_run = nr_run_min; nr_run < threshold_size; nr_run++) {
 		unsigned int nr_threshold;
 		if (eco_mode_active == 1) {
 			nr_threshold = nr_run_thresholds_eco[nr_run - 1];
